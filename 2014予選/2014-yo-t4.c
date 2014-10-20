f22278eea1be3ccb63593698983bc1c83549ae25
kazuki <kzkysgi@yahoo.co.jp>
2014-10-20 22:35:35 +0900

    2014-yo-t4できた

--- 2014予選/2014-yo-t4.c
+++ 2014予選/2014-yo-t4.c
@@ -54,20 +54,18 @@ int main(){
 
 	count[0][4]=1;	//0日目はJが鍵を持っている
 
-	/*for(i=0; i<8; i++){
-		printf("%d ",count[0][i]);
-	}*/
 	//来れる組み合わせ
 	for(i=1; i<=N; i++){
 		printf("%d:",i);
 		for(j=0; j<8; j++){
 			if(count[i-1][j]>0){	//前日のメンバー
 				for(k=0; k<8; k++){
-					if((sekinin[i-1] & k)>0){	//今日の責任者(i-1)とk(メンバーの全通り)をAND演算,else:絶対にならないメンバー
-						if((j & k)>0){	//前日のメンバーとk(メンバーの全通り)をOR演算
+					if((sekinin[i-1] & k)>0){	//今日の責任者(i-1)とk(メンバーの全通り)をAND演算,true:今日できる組み合わせ,else:絶対にならないメンバー
+						if((j & k)>0){	//前日のメンバーとk(今日できる組み合わせ)をAND演算
 							count[i][k]+=count[i-1][j];
 						}
-					}	
+						count[i][k]=count[i][k]%10007;	//10007で割る
+					}
 				}
 			}
 			printf("%d ",count[i][j]);
@@ -76,9 +74,9 @@ int main(){
 	}
 
 	for(i=0; i<8; i++){
-		printf("%d ",count[N][i]);
 		total+=count[N][i];
 	}
+	total=total%10007;
 	printf("\n%d",total);
 
 	return 0;
